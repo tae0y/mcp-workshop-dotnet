ebbb69f2658bc635deba988bd8df8c2f817abfe5
박태영(tae0y) <tae0yp@gmail.com>
2025-08-02 10:03:33 +0900

    fix : 인메모리 안됐던 이슈 해결

--- complete/src/McpTodoServer.ContainerApp/Program.cs
+++ complete/src/McpTodoServer.ContainerApp/Program.cs
@@ -6,15 +6,25 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-//var connection = new SqliteConnection("Filename=:memory:");
+/**
+* connection 객체를 직접 생성하여,
+* 여러 dbcontext 인스턴스에서 공유할 수 있습니다.
+*/
 var connection = new SqliteConnection("DataSource=:memory:");
 connection.Open();
-
 builder.Services.AddSingleton(connection);
-
 builder.Services.AddDbContext<TodoDbContext>(options => options.UseSqlite(connection));
 builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+
+/**
+* DI에서 connection 객체를 생성하여,
+* DBContext마다 별도의 connection을 사용합니다.
+* 따라서 각 요청별로 db가 공유되지 않습니다. 
+*/
+//builder.Services.AddDbContext<TodoDbContext>(options =>
+//    options.UseSqlite("DataSource=:memory:")
+//           .EnableSensitiveDataLogging()
+//);
 
 builder.Services.AddMcpServer()
                 .WithHttpTransport(o => o.Stateless = true)
